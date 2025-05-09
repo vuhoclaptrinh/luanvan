a6e242668895fd26fc464663a5a8ed640d5579b7
unknown <iamvux2002@gmail.com>
2025-05-09 18:46:47 +0700

    thêm layou notfound sanpham

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,52 @@
-import "./main/App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme";
+
+// Gọi các component
+import Layout from "./components/Layout";
+import SanphamList from "./components/Sanpham/SanphamList";
+import NotFound from "./components/Notfound";
+
+const PrivateRoute = ({ children }) => {
+  const user = localStorage.getItem("user");
+  return user ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          {/* Route chính dùng Layout bọc */}
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Layout />
+              </PrivateRoute>
+            }
+          >
+            {/* Đây là các route con nằm trong Layout */}
+            <Route
+              index
+              element={<h1>Trang chủ - thêm /sanpham để xem sản phẩm</h1>}
+            />
+            <Route path="sanpham" element={<SanphamList />} />
+          </Route>
+
+          {/* Route cho các path không khớp */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
